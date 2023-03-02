f6e609bf1a0d958af15ea3bd3437df1b93ac6eb8
Daniel Hoang <daniel@akuity.io>
2023-03-02 13:16:41 -0600

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("<USERNAME> is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("dhpup is Awesome!!!", "larry3d", "yellow", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
